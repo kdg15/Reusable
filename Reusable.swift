cdd8868bac6a41617f8b8c30367454ddea99a284
bwk <brian@bardownsoftware.com>
2017-03-14 21:41:31 -0400

    Initial commit.

---
+++ Reusable.swift
@@ -0,0 +1,31 @@
+//
+//  Components
+//
+//  Created by bwk on Sun.07.Mar.17.
+//  Copyright © 2017 Bar Down Software. All rights reserved.
+//
+
+import UIKit
+
+protocol Reusable: class {}
+
+extension Reusable where Self: UIView
+{
+    static var reuseIdentifier: String
+    {
+        return String(describing: self)
+    }
+}
+
+extension UITableView
+{
+    func register<T: UITableViewCell>(_: T.Type) where T: Reusable
+    {
+        register(T.nib()!, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+
+    func dequeueCell<T: UITableViewCell>(reuseableCell : T.Type) -> T? where T: Reusable
+    {
+        return dequeueReusableCell(withIdentifier: reuseableCell.reuseIdentifier) as? T
+    }
+}
